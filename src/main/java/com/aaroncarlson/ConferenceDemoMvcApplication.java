eb5a1460c170c73fbeb80d9774e20bc3f68bec83
Aaron Carlson <acarlson90@gmail.com>
2020-04-22 21:45:35 -0700

    Initial Commit

---
+++ src/main/java/com/aaroncarlson/ConferenceDemoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.aaroncarlson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceDemoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceDemoMvcApplication.class, args);
+	}
+
+}
