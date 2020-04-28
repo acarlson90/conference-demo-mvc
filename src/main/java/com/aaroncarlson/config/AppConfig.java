bbf3f483c152d3f18edf1dfd9d25fc98512afaee
Aaron Carlson <acarlson90@gmail.com>
2020-04-27 21:43:21 -0700

    PDF Support

--- src/main/java/com/aaroncarlson/config/AppConfig.java
+++ src/main/java/com/aaroncarlson/config/AppConfig.java
@@ -3,10 +3,18 @@ package com.aaroncarlson.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("/WEB-INF/pdf/");
+    }
 
     @Bean
     public ViewResolver viewResolver() {
